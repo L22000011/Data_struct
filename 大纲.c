aa3e57a02486c4af4d3ac0b601072a8af9e7b17c
Eagle <78003801+L22000011@users.noreply.github.com>
2022-06-04 12:18:40 +0800

    Update 大纲.c

--- 大纲.c
+++ 大纲.c
@@ -110,15 +110,22 @@ Void add(poly &pa,poly &pb)
 poly qa,qb,q;
 qa=pa->next;q=pa;qb=pb->next;
 while(qa&&qb)
-if(qa->exp<qb->exp){q=qa;qa=qa->next}
+if(qa->exp<qb->exp){
+  q=qa;
+  qa=qa->next;
+  }
 else if(qa->exp==qb->exp)
-{ sum= qa->coef+qb->coef;
+{ 
+  sum= qa->coef+qb->coef;
 pb->next=qb->next;
 free(qb);qb=pb->next;
-if(sum==0){ q->next=qa->next;
-free(qa);qa=q->next; }
-else{qa->coef=sum;q=qa;qa=qa->next;}
+  if(sum==0){ q->next=qa->next;
+  free(qa);qa=q->next; 
+            }
+else  {qa->coef=sum;q=qa;qa=qa->next; 
+      }
 }
+  
 else{ pb->next=qb->next;
 qb->next=qa;q->next=qb;
 q=qb;qb=pb->next;}
@@ -128,11 +135,40 @@ free(pb);
 
 
                  第三章 
+应用：汉诺塔
+#include<stdio.h>
+ 
+void move(int n,char a,char b,char c)
+{
+    if(n==1)
+        printf("\t%c->%c\n",a,c);    //当n只有1个的时候直接从a移动到c 
+    else
+    {   
+        move(n-1,a,c,b);            //把a的n-1个盘子通过c移动到b 
+        printf("\t%c->%c\n",a,c);   //把a的最后1个盘(最大的盘)移动到c 
+        move(n-1,b,a,c);            //吧b上面的n-1个盘通过a移动到c 
+    }   
+}
+ 
+main()
+{
+    int n;
+    printf("请输入要移动的块数："); 
+    scanf("%d",&n);
+    move(n,'a','b','c');
+}；
+
+
 栈和队列
+
 掌握栈和队列的基本概念。
+
 掌握栈和队列的顺序存储结构、链式存储结构以及基本操作的实现 （算法）。
+
 --循环队列（队列的顺序存储结构）
+
 掌握栈和队列的区别 
+
 掌握栈、队列与线性表的区别
 
 
